0c94d35eac511bec35947f02024bbb0ade67a4a8
Darren Liew <ldarren@gmail.com>
2022-01-02 03:28:11 +0800

    added link chart

--- server/service/okapi/index.js
+++ server/service/okapi/index.js
@@ -3,11 +3,12 @@ const base = require('service/okapi/base.json')
 const common = require('service/common.json')
 const treeCfg = require('service/tree.json')
 const tree = require('service/tree')
+const system = require('service/system.json')
 
 const out = {}
 
 this.load = () => {
-	pObj.extends(out, [common, treeCfg, {tree}, base], {flat:1})
+	pObj.extends(out, [common, treeCfg, system, {tree}, base], {flat:1})
 }
 
 return out
